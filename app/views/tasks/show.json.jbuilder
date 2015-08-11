4cbfa994c00f0fb06491b373856ddbb71a9af306
Andrew Carmer <carmerandrew@gmail.com>
2015-08-11 11:00:17 -0600

    tdd

--- app/views/tasks/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @task, :id, :title, :status, :notes, :due_date, :start_date, :created_at, :updated_at
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-
+require 'simplecov'
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -7,6 +7,30 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    SimpleCov.start
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
 end
